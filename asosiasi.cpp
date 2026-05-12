c79ef54a2f6ff6cc7dc26180752865df5c02209f
NekoKaicho <adecta7@gmail.com>
2026-05-12 18:44:33 +0700

    Tambah kelas dokter dengan atribut nama dan daftar pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -16,3 +16,12 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
