69a08029149fabcbd1410de2a471bb972ab068d5
NekoKaicho <adecta7@gmail.com>
2026-05-12 17:50:36 +0700

    Tambah struktur kelas dan constructor di file anak.h

--- anak.h
+++ anak.h
@@ -1,2 +1,15 @@
 #ifndef ANAK_H
 #define ANAK_H
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    ~anak() {
+        COUT << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
+
