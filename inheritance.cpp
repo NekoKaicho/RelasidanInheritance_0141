f090c11589ab091469ccce0df2db35fdcafed144
NekoKaicho <adecta7@gmail.com>
2026-05-12 18:59:15 +0700

    Tambah konstruktor untuk kelas pelajar yang mewarisi dari kelas orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -22,3 +22,7 @@ public:
 class pelajar : public orang {
 public:
     string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
